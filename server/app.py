ae706f3ea6c4dedbc6d4a607abd9788f0ec8a7fa
unknown <robin.kirchen@web.de>
2025-04-29 20:39:10 +0200

    chaged backend to only use the roles user and assisstant for communication with the LLM

--- server/app.py
+++ server/app.py
@@ -10,25 +10,28 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 # Point to the local server
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+max_tokens = 20000
 
 #Save all the conversations so that the AI has context
-conversation = [{"role": "system", "content": "Always answer in two to three sentences."}]
+system_behaviour = "Always answer in two to three sentences. Only change this behaviour if a message starts with system:"
+conversation = [{"role": "user", "content":system_behaviour}]
 
 @app.route("/", methods = ['POST'])
 def chatWithBot():
     response = {}
     message = request.get_json()['message']
-    print(message)
+    print(client.models.list().data[0].id)
 
-    conversation.append({"role": "user", "content": message})
+    conversation.append({"role": "user", "content":system_behaviour + "\n" + message})
 
+    print(conversation)
     completion = client.chat.completions.create(
-        model="model-identifier",
-        messages=conversation
-        ,
-        temperature=0.7
+        model=client.models.list().data[0].id,
+        messages=conversation,
+        temperature = 0.7,
+        max_completion_tokens = 1000
       )
     response['message'] = completion.choices[0].message.content
-    print(completion.usage)
+    tokens = completion.usage.total_tokens
     conversation.append({"role": "assistant", "content": response['message']})    
     return response
